d09b855dc3d94df0140327ec0e8bbb4c8a11999c
TomFnt <tom.fnt9@gmail.com>
2024-10-21 16:54:44 +0200

    Remove usseless controller name variable in HomeController & fix label margin in inscription page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -16,7 +16,6 @@ class HomeController extends AbstractController
 
         return $this->render('home/index.html.twig', [
             'topSellingProducts' => $topSellingProducts,
-            'controller_name' => 'HomeController',
         ]);
     }
 }
