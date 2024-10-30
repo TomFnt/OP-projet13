23a36638b0a298a07fc986a853177f008eae31ec
TomFnt <tom.fnt9@gmail.com>
2024-10-31 00:36:10 +0100

    implement update quantity of an product in cart

--- src/Services/ProductOrderService.php
+++ src/Services/ProductOrderService.php
@@ -7,23 +7,35 @@ use App\Entity\Product;
 use App\Entity\ProductOrder;
 use App\Entity\User;
 use App\Repository\OrderRepository;
+use App\Repository\ProductOrderRepository;
+use App\Repository\ProductRepository;
 use Doctrine\ORM\EntityManagerInterface;
 
 
 class ProductOrderService
 {
-    public function __construct(EntityManagerInterface $em, OrderRepository $orderRepository) {
+    public function __construct(EntityManagerInterface $em, OrderRepository $orderRepository, ProductOrderRepository $productOrderRepository) {
         $this->em = $em;
         $this->orderRepository = $orderRepository;
+        $this->productOrderRepository = $productOrderRepository;
     }
 
 
     public function addToCart(Product $product, int $quantity, Order $order){
-        $productOrder = new ProductOrder();
-        $productOrder->setProduct($product);
-        $productOrder->setOrder($order);
-        $productOrder->setQuantity($quantity);
 
+        $productOrder = $this->productOrderRepository->findOneBy([
+            'order' => $order,
+            'product' => $product
+        ]);
+
+        if ($productOrder) {
+            $productOrder->setQuantity($quantity);
+        } else {
+            $productOrder = new ProductOrder();
+            $productOrder->setProduct($product);
+            $productOrder->setOrder($order);
+            $productOrder->setQuantity($quantity);
+        }
         $this->em->persist($productOrder);
         $this->em->flush();
     }
@@ -49,7 +61,6 @@ class ProductOrderService
     public function getQuantityProductInCart(User $user, Product $product){
 
         $quantity = null;
-
         $cart = $this->orderRepository->findOneBy(['user' => $user, 'status' => 'pending']);
 
         if ($cart != null) {
